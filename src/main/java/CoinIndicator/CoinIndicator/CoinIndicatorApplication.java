c1a25b34701e16a54a63df726d6d318d8f80d9bc
minjunss <jomj2214@gmail.com>
2024-08-01 01:30:38 +0900

    first commit

---
+++ src/main/java/CoinIndicator/CoinIndicator/CoinIndicatorApplication.java
@@ -0,0 +1,13 @@
+package CoinIndicator.CoinIndicator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoinIndicatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoinIndicatorApplication.class, args);
+	}
+
+}
